378c9f71c2aceec676ef17f274bf2b66ef664b39
Anietie Akpanumoh <anietieakpanumoh1@gmail.com>
2022-09-05 11:45:11 +0100

    added functionality to the rotating navigation

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const mainContainer = document.querySelector('.main-container');
 
+open.addEventListener('click', () => mainContainer.classList.add('show-nav'));
+
+close.addEventListener('click', () => mainContainer.classList.remove('show-nav'));
 
