dc82202ac0e86aee2bf7dbb1422e884e0de99e9a
Anietie Akpanumoh <anietieakpanumoh1@gmail.com>
2022-09-18 16:33:53 +0100

    fixed the issue raised

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const mainContainer = document.querySelector('.main-container');
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const mainContainer = document.querySelector(".main-container");
+const body = document.body;
 
-open.addEventListener('click', () => mainContainer.classList.add('show-nav'));
+open.addEventListener("click", () => {
+    mainContainer.classList.add("show-nav");
+    mainContainer.classList.add("noscroll");
+    body.classList.add("noscroll");
+});
 
-close.addEventListener('click', () => mainContainer.classList.remove('show-nav'));
+close.addEventListener("click", () => {
+    mainContainer.classList.remove("show-nav");
+    mainContainer.classList.remove("noscroll");
+    body.classList.remove("noscroll");
+});
+
+
+
+
 
